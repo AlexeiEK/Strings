23680f7747efad7665a0cf4bc08a928c8adb7694
Алексей <a3278cpp@yandex.ru>
2021-02-02 02:50:22 +0300

    Home_Work_30_01_02_02

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -5,6 +5,14 @@ using namespace std;
 void InputLine(char str[], const int n);
 int StrLen(char str[]);
 
+void to_upper(char str[]);
+void shrink(char str[]);
+bool is_palindrome(char str[]);
+bool is_number(char str[]);
+
+//#define ClassWork
+
+#ifdef ClassWork
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -24,6 +32,31 @@ void main()
 	cout << str << endl;
 	cout << "Длина строки: " << StrLen(str) << endl;
 }
+#endif // ClassWork
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 1500;
+	char str[n];
+	cout << "Введите строку: ";
+	InputLine(str, n);
+	system("CLS");
+	cout << str << endl;
+	shrink(str);
+	cout << str << endl;
+	cout << "Введите число: ";
+	InputLine(str, n);
+	system("CLS");
+	cout << str << endl;
+	is_number(str) ? cout << "Да - это число!" : cout << "Это не число!";
+	cout << endl;
+	cout << "Введите строку: ";
+	InputLine(str, n);
+	system("CLS");
+	cout << str << endl;
+	is_palindrome(str) ? cout << "Эта строка палиндром!" : cout << "Эта строка не является палиндромом!";
+}
+
 void InputLine(char str[], const int n)
 {
 	SetConsoleCP(1251);
@@ -36,5 +69,61 @@ int StrLen(char str[])
 	//Возвращает длину строки
 	int i = 0;
 	for (; str[i]; i++);
+	return i;
+}
+void to_upper(char str[])
+{
+
+}
+void shrink(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		//Убираем пробелы вначале строки
+		while (str[0] == ' ')
+		{
+			for (int j = 0; str[j]; j++)
+			{
+				str[j] = str[j + 1];
+			}
+		}
+		//Убираем лишние пробелы далее в строке
+		while (str[i] == ' ' && str[i + 1] == ' ')
+		{
+			for (int j = i + 1; str[j]; j++)
+			{
+				str[j] = str[j + 1];
+			}
+		}
+	}
+}
+bool is_palindrome(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		while (str[i] == ' ')
+		{
+			for (int j = i; str[j]; j++)
+			{
+				str[j] = str[j + 1];
+			}
+		}
+	}
+	int j = StrLen(str);
+	int i = 0;
+	do
+	{
+		str[i] == str[j - 1] ? i++ : i = 0;
+		j--;
+	} while (i && j);
+	return i;
+}
+bool is_number(char str[])
+{
+	int i = 0;
+	do
+	{
+		str[i] >= '0' && str[i] <= '9' || str[i] == ' ' ? i++ : i = 0;  // Пробелы можно убрать, если они не нужны
+	} while (i && str[i]);
 	return i;
 }
